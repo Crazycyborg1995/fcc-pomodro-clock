0ea6f189f29693b4af2914957e3ae520e52cb4d5
Crazycyborg1995 <mhdafsalvp@gmail.com>
2019-05-19 12:18:48 +0530

    Updates

---
+++ precache-manifest.e401442c815f4f86eba426612109bd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce1f6549a5550e70bbf7",
+    "url": "/fcc-pomodro-clock/static/css/main.4b1d9c65.chunk.css"
+  },
+  {
+    "revision": "ce1f6549a5550e70bbf7",
+    "url": "/fcc-pomodro-clock/static/js/main.ce1f6549.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/fcc-pomodro-clock/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "70a28917f16de266374c",
+    "url": "/fcc-pomodro-clock/static/js/runtime~main.70a28917.js"
+  },
+  {
+    "revision": "48b3d0782a6cf3499f5c9afe4582894c",
+    "url": "/fcc-pomodro-clock/static/media/pom-logo.48b3d078.png"
+  },
+  {
+    "revision": "419286dbc0abe6b0dab545ef16df9d07",
+    "url": "/fcc-pomodro-clock/index.html"
+  }
+];
